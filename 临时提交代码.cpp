134189a985e8bee391b8cbe2b97e45c8c2d80179
Chantenize <49093416+Chantenize@users.noreply.github.com>
2019-04-01 23:33:31 +0800

    Update 临时提交代码.cpp

--- 临时提交代码.cpp
+++ 临时提交代码.cpp
@@ -10,43 +10,44 @@ int n;
 int check;
 
 void dfs(int start){
-    //cout<<"�±�Ϊ��"<<start<<"ͷΪ��"<<head[start]<<"βΪ��"<<endd[start]<<endl;
+    //cout<<"下标为："<<start<<"头为："<<head[start]<<"尾为："<<endd[start]<<endl;
     if(endd[start]=='m'){
         check=1;
         return;
     }
-    for(int i=0; i<n; i++){
+    for(int i=0;i<n;i++){
         if(head[i]==endd[start]&&vis[i]==0){
             vis[i]=1;
             dfs(i);
             vis[i]=0;
         }
     }
+    //if(start==7) cout<<"hehe"<<endl;
+    //return;
 }
 
 int main(){
-    for(int i=1; i<=3; i++){
-        string a;
-        n=0;
-        while(cin>>a){
-            if(a[0]=='0') break;
-            head[n]=a[0];
-            endd[n]=a[a.length()-1];
-            //cout<<"n= "<<n<<" "<<head[n]<<" "<<endd[n]<<endl;
-            n++;
-        }
-        memset(vis,0,sizeof(vis));
-        check=0;
-        for(int i=0; i<n; i++){
-            if(head[i]=='b'){
-                vis[i]=1;
-                dfs(i);
-                vis[i]=0;
-            }
-            if(check==1) break;
-        }
-        if(check==0) cout<<"No."<<endl;
-        if(check==1) cout<<"Yes."<<endl;
+    string a;
+    n=0;
+    while(cin>>a){
+        if(a[0]=='0') break;
+        head[n]=a[0];
+        endd[n]=a[a.length()-1];
+        //cout<<"n= "<<n<<" "<<head[n]<<" "<<endd[n]<<endl;
+        n++;
     }
+    //cout<<"n= "<<n<<endl;
+    memset(vis,0,sizeof(vis));
+    check=0;
+    for(int i=0;i<n;i++){
+        if(head[i]=='b'){
+            vis[i]=1;
+            dfs(i);
+            vis[i]=0;
+        }
+        if(check==1) break;
+    }
+    if(check==0) cout<<"No."<<endl;
+    if(check==1) cout<<"Yes."<<endl;
     return 0;
 }
